ef92938d81470db7b4be25c7997b9a89032296fa
Xavier Watson <xmwatson@memphis.edu>
2024-02-29 01:21:43 -0600

    Added a limited tries mechanic to guessing game

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,13 @@
+lives = 3
 number = 10
+while lives > 0:
+   print("I'm thinking of a number...")
+   guess = int(input("What number am I thinking of? "))
 
-print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-if guess > number:
-   print("Sorry, your guess was too high, the number was", number)
-elif guess < number:
-   print("Sorry, your guess was too low, the number was", number)
-else:
-   print("Congratulations! You guessed the right number.")
+   if guess == number:
+      print("Congratulations! You guessed the right number.")
+   else:
+      print(f"Sorry! The number was {number}.")
+      lives -= 1
+if lives == 0:
+   print("Sorry no more guesses for you :(")
