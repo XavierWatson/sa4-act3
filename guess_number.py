2b5ed5f1f6929e6378f9b538cb2982b408336d59
Xavier Watson <xmwatson@memphis.edu>
2024-02-29 01:15:28 -0600

    Added hint message to python guessing game

--- guess_number.py
+++ guess_number.py
@@ -2,8 +2,9 @@ number = 10
 
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
-
-if guess == number:
-   print("Congratulations! You guessed the right number.")
+if guess > number:
+   print("Sorry, your guess was too high, the number was", number)
+elif guess < number:
+   print("Sorry, your guess was too high, the number was", number)
 else:
-   print(f"Sorry! The number was {number}.")
+   print("Congratulations! You guessed the right number.")
