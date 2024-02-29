98f8ad3b34b49bd6c8e638f40baf220cd6c65ad0
Xavier Watson <xmwatson@memphis.edu>
2024-02-29 01:16:53 -0600

    fixed typo in [low guess] message

--- guess_number.py
+++ guess_number.py
@@ -5,6 +5,6 @@ guess = int(input("What number am I thinking of? "))
 if guess > number:
    print("Sorry, your guess was too high, the number was", number)
 elif guess < number:
-   print("Sorry, your guess was too high, the number was", number)
+   print("Sorry, your guess was too low, the number was", number)
 else:
    print("Congratulations! You guessed the right number.")
